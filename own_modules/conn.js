7a45bcdf0e2b6b75d9e8efbefc57f4c93458576f
alinaugustin <alindebian@gmail.com>
2018-12-28 19:51:31 +0200

    Update conn.js

--- own_modules/conn.js
+++ own_modules/conn.js
@@ -1,10 +1,10 @@
 
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-              host     : 'localhost',
-              user     : 'root',
-              password : 'testpass',
-              database : 'app1'
+              host     : 'ec2-46-137-99-175.eu-west-1.compute.amazonaws.com',
+              user     : 'ywitwkxvyevnxv',
+              password : 'fa37b6048a879a5f303d22cf216adc0453b0083b43cffb57795a781afa698f3d',
+              database : 'd1qrkjj03t8v6f'
             });
 //var connection = conn;
 connection.connect();
